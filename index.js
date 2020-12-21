5c2405d1071f7b50e8bf67afb2a94501618dec7d
Ally Kadel <akadel01@gmail.com>
2020-12-21 21:50:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function findAdjacent(rootNode, vertices, edges){
   let res = edges.filter(edge => {
     edge.includes(rootNode)
   })
+  let adjacent = res.map(edge => {
+    if (edge[0] === rootNode){
+      return edge[0]
+    }
+    if (edge[1] === rootNode){
+      return edge[1]
+    }
+  })
+  return adjacent
 }
 
 function markDistanceAndPredecessor(){
