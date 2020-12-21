555718837e2e3ce7e1f0ae1345bdb8f625b2db77
Ally Kadel <akadel01@gmail.com>
2020-12-21 21:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function bfs(rootNode, vertices, edges){
 
 function findAdjacent(rootNode, vertices, edges){
   let res = edges.filter(edge => {
-    console.log(edge)
-    console.log(rootNode)
     return edge.includes(rootNode)
   })
   console.log(res)
