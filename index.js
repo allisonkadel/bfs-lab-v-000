e8bab6febfdf19447195b47d6fe14295114fd34b
Ally Kadel <akadel01@gmail.com>
2020-12-21 21:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function findAdjacent(rootNode, vertices, edges){
   console.log(rootNode, vertices, edges)
   let adjacent = res.map(edge => {
     if (edge[0] === rootNode){
-      return edge[0]
+      return edge[1]
     }
     if (edge[1] === rootNode){
-      return edge[1]
+      return edge[0]
     }
   })
   return adjacent
